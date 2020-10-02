c45929469d1bcc78a007c7edb40aca710fc10909
amanymous <aman.mirza358@gmail.com>
2020-10-02 17:10:31 +0500

    debugging done

--- 3)dev-debugging/dev-debugging-/assets/scripts/vendor.js
+++ 3)dev-debugging/dev-debugging-/assets/scripts/vendor.js
@@ -1,14 +1,13 @@
-const userInput = document.getElementById('input-number');
+const usrInput = document.getElementById('input-number');
 const addBtn = document.getElementById('btn-add');
-const subBtn = document.getElementById('btn-sub');
-const multBtn = document.getElementById('btn-mult');
-const dividBtn = document.getElementById('btn-divid');
+const subtractBtn = document.getElementById('btn-subtract');
+const multiplyBtn = document.getElementById('btn-multiply');
+const divideBtn = document.getElementById('btn-divide');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
-function OutputResult(result, text) {
+function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
-
